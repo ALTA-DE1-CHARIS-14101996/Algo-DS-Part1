d5e2e14b9d229ab032df3e3487b6743d6354beda
Charis Chakim <132967245+CharisChakim@users.noreply.github.com>
2023-10-09 14:49:58 +0700

    main.py
    little change forget to -delete comment (berisi trial code)

--- problem4/main.py
+++ problem4/main.py
@@ -8,43 +8,6 @@ def isPrime(n):
     return True
 
 def generate_primes_grid(width, height, start):
-    # grid = []
-    # start_prime = start + 1
-    # for i in range(height):
-    #     row = []
-    #     for j in range(width):
-    #         while not isPrime(start_prime):
-    #             start_prime += 1
-    #         row.append(start_prime)
-    #         start_prime += 1
-    #     grid.append(row)
-    
-    # result = ""
-    # if len(str(row)) == 1:
-    #     for row in grid:
-    #         row_str = "  ".join(map(str, row))
-    #         result += row_str + "\n"  
-    # else:
-    #     for row in grid:
-    #         row_str = " ".join(map(str, row))
-    #         result += row_str + "\n"  
-    # return result
-    # result = ""
-    # for i in range(start, start + height):
-    #     row = []
-    #     start = start + 1
-    #     for j in range(width):
-    #         while not isPrime(start):
-    #             start += 1
-    #         row.append(start)
-    #         start += 1
-    #     if len(str(row[0])) == 1:
-    #         row_str = "  ".join(map(str, row))
-    #     else:
-    #         row_str = " ".join(map(str, row))
-    #     result += row_str + "\n"
-    # return result
-    
     result = ""
     start = start + 1
     for i in range(start, start + height):
